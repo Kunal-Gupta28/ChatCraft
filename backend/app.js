8c32bbd191876dfb387df3ed96ce1e5153cb9388
Kunal-Gupta28 <kunal.gmail.91165@gmail.com>
2025-11-19 22:59:09 +0530

    add cors's alloworigin

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,24 @@
-const express = require('express')
-const app = express()
-const userRouter = require('./routes/user.router')
-const projectRouter = require('./routes/project.route')
-const aiRouter = require('./routes/ai.route')
-const connectToDB = require('./config/connectToDb')
-const cors = require('cors')
-connectToDB()
+const express = require("express");
+const app = express();
+const userRouter = require("./routes/user.router");
+const projectRouter = require("./routes/project.route");
+const aiRouter = require("./routes/ai.route");
+const connectToDB = require("./config/connectToDb");
+const cors = require("cors");
+connectToDB();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, process.env.CLIENT_URL_DEV],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cors())
+app.use("/", userRouter);
+app.use("/project", projectRouter);
+app.use("/ai", aiRouter);
 
-
-app.use('/',userRouter)
-app.use('/project',projectRouter)
-app.use('/ai',aiRouter)
-
-
-module.exports = app;  
+module.exports = app;
